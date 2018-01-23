acbaf7386bd9d6d6ff58684ac928693059df0ef8
nzbin <nzb329@163.com>
2018-01-23 23:28:38 +0800

    Release 0.4.0

--- docs/js/jquery.magnify.js
+++ docs/js/jquery.magnify.js
@@ -6,7 +6,7 @@
  * |  |  |  |   _   |  \_/   |  |\   |_| |_|  |      |  |
  * |__|  |__|__| |__|\____/|_|__| \__|_____|__|      |__|
  *
- * jquery.magnify.js v0.3.2
+ * jquery.magnify.js v0.4.0
  *
  * A jQuery plugin to view images just like in windows
  *
