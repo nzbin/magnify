73e48267d79b39b1a3a899c438a517de5a7f7748
nzbin <nzb329@163.com>
2018-07-31 22:00:21 +0800

    chore(release): v1.1.0

--- dist/jquery.magnify.js
+++ dist/jquery.magnify.js
@@ -6,7 +6,7 @@
  * |  |  |  |   _   |  \_/   |  |\   |_| |_|  |      |  |
  * |__|  |__|__| |__|\____/|_|__| \__|_____|__|      |__|
  *
- * jquery.magnify - v1.0.0
+ * jquery.magnify - v1.1.0
  * A jQuery plugin to view images just like in windows
  * https://github.com/nzbin/magnify#readme
  *
@@ -29,6 +29,169 @@
 })(function($) {
 
   'use strict';
+
+/**
+ * Private Functions
+ */
+
+/**
+ * [getImgSrc]
+ * @param {[Object]}  el    [description]
+ */
+function getImgSrc(el) {
+  // Get data-src as image src at first
+  var src = $(el).attr('data-src') ? $(el).attr('data-src') : $(el).attr('href');
+  return src;
+}
+
+/**
+ * [throttle]
+ * @param  {Function} fn    [description]
+ * @param  {[Number]} delay [description]
+ * @return {Function}       [description]
+ */
+function throttle(fn, delay) {
+
+  var timer = null;
+
+  return function () {
+    var context = this,
+      args = arguments;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+
+}
+
+/**
+ * [preloadImg]
+ * @param  {[String]}  src      [image src]
+ * @param  {Function}  success  [callbacks]
+ * @param  {Function}  error    [callbacks]
+ */
+function preloadImg(src, success, error) {
+
+  var img = new Image();
+
+  img.onload = function () {
+    success(img);
+  };
+
+  img.onerror = function () {
+    error(img);
+  };
+
+  img.src = src;
+
+}
+
+/**
+ * [requestFullscreen]
+ * @param  {[type]} element [description]
+ */
+function requestFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+/**
+ * [exitFullscreen]
+ */
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+/**
+ * [getImageNameFromUrl]
+ * @param  {[String]} url [description]
+ * @return {[String]}     [description]
+ */
+function getImageNameFromUrl(url) {
+  var reg = /^.*?\/*([^/?]*)\.[a-z]+(\?.+|$)/ig,
+    txt = url.replace(reg, '$1');
+  return txt;
+}
+
+/**
+ * [getNumFromCSSValue]
+ * @param  {[String]} value [description]
+ * @return {[Number]}       [description]
+ */
+function getNumFromCSSValue(value) {
+  var reg = /\d+/g,
+    arr = value.match(reg),
+    num = parseFloat(arr[0]);
+  return num;
+}
+
+/**
+ * [hasScrollbar]
+ * @return {[Boolean]}       [description]
+ */
+function hasScrollbar() {
+  return document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight);
+}
+
+/**
+ * [getScrollbarWidth]
+ * @return {[Number]}       [description]
+ */
+function getScrollbarWidth() {
+
+  var scrollDiv = document.createElement('div');
+  scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
+  document.body.appendChild(scrollDiv);
+  var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+  document.body.removeChild(scrollDiv);
+
+  return scrollbarWidth;
+
+}
+
+/**
+ * [setGrabCursor]
+ * @param {[Object]}  imageData    [description]
+ * @param {[Object]}  stageData    [description]
+ * @param {[Object]}  stage        [description]
+ * @param {[Boolean]} isRotate     [description]
+ */
+function setGrabCursor(imageData, stageData, stage, isRotated) {
+
+  var imageWidth = !isRotated ? imageData.w : imageData.h,
+    imageHeight = !isRotated ? imageData.h : imageData.w;
+
+  if (imageHeight > stageData.h || imageWidth > stageData.w) {
+    stage.addClass('is-grab');
+  }
+  if (imageHeight <= stageData.h && imageWidth <= stageData.w) {
+    stage.removeClass('is-grab');
+  }
+}
+
+/**
+ * [supportTouch]
+ * @return {[Boolean]}     [description]
+ */
+function supportTouch() {
+  return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+}
 
 /**
  * Private Static Constants
@@ -1616,168 +1779,5 @@ var resizable = function(modal, stage, image, minWidth, minHeight) {
 $.extend(Magnify.prototype, {
   resizable: resizable
 });
-
-/**
- * Private Functions
- */
-
-/**
- * [getImgSrc]
- * @param {[Object]}  el    [description]
- */
-function getImgSrc(el) {
-  // Get data-src as image src at first
-  var src = $(el).attr('data-src') ? $(el).attr('data-src') : $(el).attr('href');
-  return src;
-}
-
-/**
- * [throttle]
- * @param  {Function} fn    [description]
- * @param  {[Number]} delay [description]
- * @return {Function}       [description]
- */
-function throttle(fn, delay) {
-
-  var timer = null;
-
-  return function () {
-    var context = this,
-      args = arguments;
-
-    clearTimeout(timer);
-
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-
-}
-
-/**
- * [preloadImg]
- * @param  {[String]}  src      [image src]
- * @param  {Function}  success  [callbacks]
- * @param  {Function}  error    [callbacks]
- */
-function preloadImg(src, success, error) {
-
-  var img = new Image();
-
-  img.onload = function () {
-    success(img);
-  };
-
-  img.onerror = function () {
-    error(img);
-  };
-
-  img.src = src;
-
-}
-
-/**
- * [requestFullscreen]
- * @param  {[type]} element [description]
- */
-function requestFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  }
-}
-
-/**
- * [exitFullscreen]
- */
-function exitFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  }
-}
-
-/**
- * [getImageNameFromUrl]
- * @param  {[String]} url [description]
- * @return {[String]}     [description]
- */
-function getImageNameFromUrl(url) {
-  var reg = /^.*?\/*([^/?]*)\.[a-z]+(\?.+|$)/ig,
-    txt = url.replace(reg, '$1');
-  return txt;
-}
-
-/**
- * [getNumFromCSSValue]
- * @param  {[String]} value [description]
- * @return {[Number]}       [description]
- */
-function getNumFromCSSValue(value) {
-  var reg = /\d+/g,
-    arr = value.match(reg),
-    num = parseFloat(arr[0]);
-  return num;
-}
-
-/**
- * [hasScrollbar]
- * @return {[Boolean]}       [description]
- */
-function hasScrollbar() {
-  return document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight);
-}
-
-/**
- * [getScrollbarWidth]
- * @return {[Number]}       [description]
- */
-function getScrollbarWidth() {
-
-  var scrollDiv = document.createElement('div');
-  scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
-  document.body.appendChild(scrollDiv);
-  var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-  document.body.removeChild(scrollDiv);
-
-  return scrollbarWidth;
-
-}
-
-/**
- * [setGrabCursor]
- * @param {[Object]}  imageData    [description]
- * @param {[Object]}  stageData    [description]
- * @param {[Object]}  stage        [description]
- * @param {[Boolean]} isRotate     [description]
- */
-function setGrabCursor(imageData, stageData, stage, isRotated) {
-
-  var imageWidth = !isRotated ? imageData.w : imageData.h,
-    imageHeight = !isRotated ? imageData.h : imageData.w;
-
-  if (imageHeight > stageData.h || imageWidth > stageData.w) {
-    stage.addClass('is-grab');
-  }
-  if (imageHeight <= stageData.h && imageWidth <= stageData.w) {
-    stage.removeClass('is-grab');
-  }
-}
-
-/**
- * [supportTouch]
- * @return {[Boolean]}     [description]
- */
-function supportTouch() {
-  return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
-}
 
 });
