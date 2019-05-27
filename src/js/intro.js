c54aff447b4052450d79725b67e4e3daabd9b955
nzbin <nzb329@163.com>
2019-05-27 23:34:49 +0800

    release: v1.4.3

--- src/js/intro.js
+++ src/js/intro.js
@@ -6,7 +6,7 @@
  * |  |  |  |   _   |  \_/   |  |\   |_| |_|  |      |  |
  * |__|  |__|__| |__|\____/|_|__| \__|_____|__|      |__|
  *
- * jquery.magnify - v1.4.2
+ * jquery.magnify - v1.4.3
  * A jQuery plugin to view images just like in windows
  * https://github.com/nzbin/magnify#readme
  *
